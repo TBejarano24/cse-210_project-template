b9cad92d2bc72698903d6734728b5c92618bf23e
TBejarano24 <bejaranotomas04@gmail.com>
2025-10-27 22:51:09 -0300

    Completed Week01 Exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic; // It's not necessary, but it is necessary!
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> userNumbers = new List<int>();
+        int intInput;
+        int numbersSum = 0;
+        double numbersAvg;
+        int numbersMax;
+
+        Console.WriteLine("Enter a series of numbers, write 0 when you're done");
+
+        do
+        {
+
+            Console.Write("Enter a number: ");
+            string strInput = Console.ReadLine();
+            intInput = int.Parse(strInput);
+
+            if (intInput != 0)
+            {
+                userNumbers.Add(intInput);
+            }
+
+        } while (intInput != 0);
+
+        foreach (int number in userNumbers)
+        {
+            numbersSum += number;
+        }
+
+        numbersAvg = (float)numbersSum / userNumbers.Count;
+        numbersMax = userNumbers.Max();
+
+        Console.WriteLine($"The sum is: {numbersSum}");
+        Console.WriteLine($"The average is: {numbersAvg:F2}");
+        Console.WriteLine($"The largest number is: {numbersMax}");
     }
 }
