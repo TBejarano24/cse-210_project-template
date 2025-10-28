1d57f214aa1b2640b5870bb6e3cd59c83a276a07
TBejarano24 <bejaranotomas04@gmail.com>
2025-10-27 21:40:09 -0300

    Completed Week01 Exercise 3 with stretch challenges

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool answer;
+
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int intGuess;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string stringGuess = Console.ReadLine();
+                intGuess = int.Parse(stringGuess);
+
+                if (intGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (intGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            } while (intGuess != magicNumber);
+
+            Console.Write("Would you like to play again? (yes/no) ");
+            string stringAnswer = Console.ReadLine();
+
+            if (stringAnswer == "yes")
+            {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+            }
+
+        } while (answer == true);
+
     }
 }
