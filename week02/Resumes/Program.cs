1a61d1cc0381e0510a2acaacac71b60b0a756dee
TBejarano24 <bejaranotomas04@gmail.com>
2025-11-03 18:45:55 -0300

    Completed Week02 Resumes assignment.

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Real Company";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+
+        Job job2 = new();
+
+        job2._jobTitle = "Data Tester";
+        job2._company = "False Company";
+        job2._startYear = 2019;
+        job2._endYear = 2020;
+
+        Resume myResume = new();
+
+        myResume._name = "Tomas Bejarano";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
