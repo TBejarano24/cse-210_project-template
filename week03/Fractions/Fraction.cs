91c48a7503771ee6c4294af95c7871a209d26127
TBejarano24 <bejaranotomas04@gmail.com>
2025-11-10 15:35:14 -0300

    Finished Week03 fractions exercise

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,60 @@
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int WholeNumber)
+    {
+        _top = WholeNumber;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        double fractionDouble;
+
+        if (_bottom != 0)
+        {
+            fractionDouble = (float)_top / _bottom;
+        }
+        else
+        {
+            fractionDouble = _top;
+        }
+
+        return fractionDouble;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Console.WriteLine("Testing constructors:");
+        Console.WriteLine("");
+
+        Fraction defaultFraction = new();
+        Fraction wholeFraction = new(6);
+        Fraction trueFraction = new(6, 7);
+
+        Console.WriteLine(defaultFraction.GetFractionString());
+        Console.WriteLine(wholeFraction.GetFractionString());
+        Console.WriteLine(trueFraction.GetFractionString());
+
+        Console.WriteLine("");
+
+        Console.WriteLine("Testing setters and getters:");
+        Console.WriteLine("");
+
+        Fraction additionalFraction = new();
+
+        defaultFraction.SetTop(1);
+        defaultFraction.SetBottom(1);
+
+        wholeFraction.SetTop(5);
+        wholeFraction.SetBottom(1);
+
+        trueFraction.SetTop(3);
+        trueFraction.SetBottom(4);
+
+        additionalFraction.SetTop(1);
+        additionalFraction.SetBottom(3);
+
+        Console.WriteLine(defaultFraction.GetFractionString());
+        Console.WriteLine($"{defaultFraction.GetDecimalValue():F2}");
+
+        Console.WriteLine(wholeFraction.GetFractionString());
+        Console.WriteLine($"{wholeFraction.GetDecimalValue():F2}");
+
+        Console.WriteLine(trueFraction.GetFractionString());
+        Console.WriteLine($"{trueFraction.GetDecimalValue():F2}");
+
+        Console.WriteLine(additionalFraction.GetFractionString());
+        Console.WriteLine($"{additionalFraction.GetDecimalValue():F2}");
     }
 }
