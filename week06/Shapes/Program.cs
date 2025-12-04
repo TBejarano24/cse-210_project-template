452cc802659d22178a434319a2157d8806c5c560
TBejarano24 <bejaranotomas04@gmail.com>
2025-12-04 00:35:49 -0300

    Completed Week06 Shapes assignment

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new("Purple", 5);
+        Rectangle rectangle = new("Red", 10, 4);
+        Circle circle = new("Green", 3);
+
+        List<Shape> shapes = [square, rectangle, circle];
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape} statistics");
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine($"{shape.GetArea():F2}");
+
+            Console.WriteLine("");
+        }
     }
 }
